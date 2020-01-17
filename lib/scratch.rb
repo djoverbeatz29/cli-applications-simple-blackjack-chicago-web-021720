768a04a7b9031743afd587bb051c3e3a1ea0aadb
Jack Overby <jroverby92@gmail.com>
2020-01-17 16:59:48 +0000

    Automatically backed up by Learn

--- lib/scratch.rb
+++ lib/scratch.rb
@@ -1,5 +1,12 @@
 def hit
   loop do {
     print "Enter 'h' to hit or 's' to stay: "
-    
-  
+    resp = gets.chomp
+    if resp == 'h'
+      return true
+    elsif resp == 's'
+      return false
+    else
+      put "Invalid response"
+  }
+end
